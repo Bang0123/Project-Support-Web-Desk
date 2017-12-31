a8816c7079c66173f6aaa215835d0c2ca33c93ba
Bang0123 <nikolajbang1995@gmail.com>
2017-12-31 01:32:15 +0100

    changes made to login component test

--- supportWebApp/src/app/components/login-page/login-page.component.spec.ts
+++ supportWebApp/src/app/components/login-page/login-page.component.spec.ts
@@ -5,51 +5,49 @@ import { TestModuleModule } from '../../modules/test-module/test-module.module';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('LoginPageComponent', () => {
-  let component: LoginPageComponent;
-  let fixture: ComponentFixture<LoginPageComponent>;
+    let component: LoginPageComponent;
+    let fixture: ComponentFixture<LoginPageComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [LoginPageComponent],
-      imports: [
-        TestModuleModule
-      ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    })
-      .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [LoginPageComponent],
+            imports: [
+                TestModuleModule
+            ],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginPageComponent);
-    fixture.detectChanges();
-    component = fixture.componentInstance;
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoginPageComponent);
+        fixture.detectChanges();
+        component = fixture.componentInstance;
+    });
 
-  it('should create the component', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create the component', () => {
+        expect(component).toBeTruthy();
+    });
 
-  it('should render h2 for login form', () => {
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('div form h2').innerHTML).toBe('Login');
-  });
+    it('should render h2 for login form', () => {
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('div form h2').innerHTML)
+            .toBe('Login');
+    });
 
-  it('should render username input for login form', () => {
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('div form input[name="username"]')).toBeTruthy();
-  });
+    it('should render inputs for login form', () => {
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('div form input[name="username"]'))
+            .toBeTruthy();
+        expect(compiled.querySelector('div form input[name="password"]'))
+            .toBeTruthy();
+    });
 
-  it('should render password input for login form', () => {
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('div form input[name="password"]')).toBeTruthy();
-  });
-
-  it('should render login button for login form', () => {
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('div form button')).toBeTruthy();
-  });
+    it('should render login button for login form', () => {
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('div form button')).toBeTruthy();
+    });
 });
